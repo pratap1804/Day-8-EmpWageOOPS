922a0d4d0bea0fb7ae9399c3483cc933a4d327e2
pratap1804 <pratap.1804@gmail.com>
2021-06-28 06:22:50 +0530

    Added: Working day in a month is 20 hence calculating monthly wage

--- EmpWage/src/Employee.java
+++ EmpWage/src/Employee.java
@@ -4,6 +4,8 @@ public class Employee {
 	static int Wage_Per_Hour =20;
 	static int Full_Day_Hour =8;
 	static int PartTime_Day_Hour =4;
+	static int Working_Day_Per_Month =20;
+	
 	
 	public static void main(String[] args) {
 			System.out.println("Welcome To Employee Wage Computation Problem");
@@ -19,14 +21,14 @@ public class Employee {
 	
 	case 1:
 		System.out.println("Employee is present");
-		int Total_Wage = Wage_Per_Hour*Full_Day_Hour;
-		System.out.println("Daily Full Day Wage is: "+Total_Wage);
+		int Total_Wage = Wage_Per_Hour*Full_Day_Hour*Working_Day_Per_Month;
+		System.out.println("Monthly Full Day Wage is: "+Total_Wage);
 		break;
 		
 	case 2:
 		System.out.println("Employee is part-time present");
-		int Total_Wage1 = Wage_Per_Hour*PartTime_Day_Hour;
-		System.out.println("Daily PartTime Wage is: "+Total_Wage1);
+		int Total_Wage1 = Wage_Per_Hour*PartTime_Day_Hour*Working_Day_Per_Month;
+		System.out.println("Monthly PartTime Wage is: "+Total_Wage1);
 		break;
 			}
 	
