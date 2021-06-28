09bfa25b3e39e89e9ec9503aabba86827bc22d27
pratap1804 <pratap.1804@gmail.com>
2021-06-28 06:06:27 +0530

    Added: WagePerHour and FullDayHours hence calculating DailyWage

--- EmpWage/src/Employee.java
+++ EmpWage/src/Employee.java
@@ -1,6 +1,9 @@
 
 public class Employee {
 	
+	static int Wage_Per_Hour =20;
+	static int Full_Day_Hour =8;
+	
 	
 	public static void main(String[] args) {
 			System.out.println("Welcome To Employee Wage Computation Problem");
@@ -11,6 +14,8 @@ public class Employee {
 		System.out.println("Employee is absent");
 	} else {
 		System.out.println("Employee is present");
+		int Total_Wage = Wage_Per_Hour*Full_Day_Hour;
+		System.out.println("Daily Full Day Wage is: "+Total_Wage);
 		}
 	}
 	
