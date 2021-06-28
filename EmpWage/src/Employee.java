88b290ac03feff0c98e13141309428dda4c30f6b
pratap1804 <pratap.1804@gmail.com>
2021-06-28 05:58:22 +0530

    Added: Random function to check fot attendance of the employees

--- EmpWage/src/Employee.java
+++ EmpWage/src/Employee.java
@@ -1,8 +1,17 @@
 
 public class Employee {
-
+	
+	
 	public static void main(String[] args) {
-	System.out.println("Welcome To Employee Wage Computation Problem");
+			System.out.println("Welcome To Employee Wage Computation Problem");
+	
+	int attendance =(int)Math.floor((Math.random()*10)%2);
+	
+	if(attendance == 0) {
+		System.out.println("Employee is absent");
+	} else {
+		System.out.println("Employee is present");
+		}
 	}
-
+	
 }
